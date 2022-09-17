f2da21c38b39de6af8dfcd5471a087d9f120fd08
Daniel Conte <182736@upf.br>
2022-09-17 03:23:54 -0300

    feat(testes): :sparkles: adiciona teste de fluxo e validacoes de pessoa

---
+++ src/__tests__/pessoa/pessoa.spec.ts
@@ -0,0 +1,171 @@
+import PessoaRepositoryInMemory from "@repositories/PessoaRepository/PessoaRepositoryInMemory";
+import CreatePessoaController from "@controllers/Pessoa/CreatePessoaController";
+import ListPessoaController from "@controllers/Pessoa/ListPessoaController";
+import { CreatePessoaDTO, UpdatePessoaDTO } from "@modelTypes/pessoa";
+import UpdatePessoaController from "@controllers/Pessoa/UpdatePessoaController";
+import GetPessoaController from "@controllers/Pessoa/GetPessoaController";
+import DeletePessoaController from "@controllers/Pessoa/DeletePessoaController";
+
+const pessoaRepoInMemory = new PessoaRepositoryInMemory();
+const createPessoaController = new CreatePessoaController(pessoaRepoInMemory);
+const listPessoaController = new ListPessoaController(pessoaRepoInMemory);
+const updatePessoaController = new UpdatePessoaController(pessoaRepoInMemory);
+const getPessoaController = new GetPessoaController(pessoaRepoInMemory);
+const deletePessoaController = new DeletePessoaController(pessoaRepoInMemory);
+
+describe("Pessoa Fluxo", () => {
+  it("Deve retornar listagem vazia", async () => {
+    const res = await listPessoaController.exec();
+
+    expect(res).toEqual([]);
+  });
+
+  it("Deve registrar um usuario", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste1",
+      email: "teste1@teste.com",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    const res = await createPessoaController.exec(pessoa);
+
+    expect(res).toEqual("Usuário cadastrado com sucesso");
+  });
+
+  it("Deve retornar o usuario cadastrado", async () => {
+    const res = await getPessoaController.exec(1);
+
+    expect(res).toEqual(
+      expect.objectContaining({
+        codigo: 1,
+        nome: "Teste1",
+        email: "teste1@teste.com",
+        senha: "123",
+        telefone: "99912345678",
+        admin: 0,
+        criadoEm: expect.any(Date),
+        atualizadoEm: expect.any(Date),
+      })
+    );
+  });
+
+  it("Deve retornar listagem com 1 pessoa", async () => {
+    const res = await listPessoaController.exec();
+
+    expect(res).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          codigo: 1,
+          nome: "Teste1",
+          email: "teste1@teste.com",
+          senha: "123",
+          telefone: "99912345678",
+          admin: 0,
+        }),
+      ])
+    );
+    expect(res.length).toEqual(1);
+  });
+
+  it("Deve registrar outra pessoa", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste2",
+      email: "teste2@teste.com",
+      senha: "456",
+      telefone: "99912312312",
+      admin: 1,
+    };
+
+    const res = await createPessoaController.exec(pessoa);
+
+    expect(res).toEqual("Usuário cadastrado com sucesso");
+  });
+
+  it("Deve retornar listagem com 2 pessoas", async () => {
+    const res = await listPessoaController.exec();
+
+    expect(res).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          codigo: expect.any(Number),
+          nome: expect.any(String),
+          email: expect.any(String),
+          senha: expect.any(String),
+          telefone: expect.any(String),
+          admin: expect.any(Number),
+        }),
+      ])
+    );
+    expect(res.length).toEqual(2);
+  });
+
+  it("Deve alterar o primeiro usuario", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 1,
+      nome: "Teste1Alt",
+      email: "teste1Alt@teste.com",
+      senha: "123456",
+      telefone: "999777777777",
+      admin: 1,
+    };
+
+    const res = await updatePessoaController.exec(pessoa);
+
+    expect(res).toEqual("Usuário alterado com sucesso");
+  });
+
+  it("Deve retornar o usuario alterado", async () => {
+    const res = await getPessoaController.exec(1);
+
+    expect(res).toEqual(
+      expect.objectContaining({
+        codigo: 1,
+        nome: "Teste1Alt",
+        email: "teste1Alt@teste.com",
+        senha: "123456",
+        telefone: "999777777777",
+        admin: 1,
+        criadoEm: expect.any(Date),
+        atualizadoEm: expect.any(Date),
+      })
+    );
+  });
+
+  it("Deve excluir o primeiro usuario", async () => {
+    const res = await deletePessoaController.exec(1);
+
+    expect(res).toEqual("Usuário excluído com sucesso");
+  });
+
+  it("Deve retornar listagem com 1 pessoa", async () => {
+    const res = await listPessoaController.exec();
+
+    expect(res).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          codigo: expect.any(Number),
+          nome: expect.any(String),
+          email: expect.any(String),
+          senha: expect.any(String),
+          telefone: expect.any(String),
+          admin: expect.any(Number),
+        }),
+      ])
+    );
+    expect(res.length).toEqual(1);
+  });
+
+  it("Deve excluir o último usuario", async () => {
+    const res = await deletePessoaController.exec(2);
+
+    expect(res).toEqual("Usuário excluído com sucesso");
+  });
+
+  it("Deve retornar listagem vazia", async () => {
+    const res = await listPessoaController.exec();
+
+    expect(res).toEqual([]);
+  });
+});
---
+++ src/__tests__/pessoa/validacoes.spec.ts
@@ -0,0 +1,238 @@
+import { CreatePessoaDTO, UpdatePessoaDTO } from "@modelTypes/pessoa";
+import PessoaRepositoryInMemory from "@repositories/PessoaRepository/PessoaRepositoryInMemory";
+import CreatePessoaController from "@controllers/Pessoa/CreatePessoaController";
+import UpdatePessoaController from "@controllers/Pessoa/UpdatePessoaController";
+import GetPessoaController from "@controllers/Pessoa/GetPessoaController";
+import DeletePessoaController from "@controllers/Pessoa/DeletePessoaController";
+
+const pessoaRepoInMemory = new PessoaRepositoryInMemory();
+const createPessoaController = new CreatePessoaController(pessoaRepoInMemory);
+const updatePessoaController = new UpdatePessoaController(pessoaRepoInMemory);
+const getPessoaController = new GetPessoaController(pessoaRepoInMemory);
+const deletePessoaController = new DeletePessoaController(pessoaRepoInMemory);
+
+describe("Pessoa Validações", () => {
+  // CREATE
+  it("Deve retornar erro de email já registrado", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste1",
+      email: "teste1@teste.com",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+      await createPessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Este e-mail já foi usado"));
+    }
+  });
+
+  it("Deve retornar erro de nome obrigatório", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "",
+      email: "teste2@teste.com",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+    } catch (error) {
+      expect(error).toEqual(Error("Nome é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de email obrigatório", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste2",
+      email: "",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+    } catch (error) {
+      expect(error).toEqual(Error("E-mail é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de senha obrigatória", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste2",
+      email: "teste2@teste.com",
+      senha: "",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+    } catch (error) {
+      expect(error).toEqual(Error("Senha é obrigatória"));
+    }
+  });
+
+  it("Deve retornar erro de telefone obrigatório", async () => {
+    const pessoa: CreatePessoaDTO = {
+      nome: "Teste2",
+      email: "teste2@teste.com",
+      senha: "123",
+      telefone: "",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+    } catch (error) {
+      expect(error).toEqual(Error("Telefone é obrigatório"));
+    }
+  });
+
+  // UPDATE
+
+  it("Deve retornar usuário não encontrado", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 3,
+      nome: "Teste1",
+      email: "teste1@teste.com",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await updatePessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Usuário não encontrado"));
+    }
+  });
+
+  it("Deve retornar erro de nome obrigatório", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 1,
+      nome: "",
+      email: "teste2@teste.com",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Nome é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de email obrigatório", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 1,
+      nome: "Teste2",
+      email: "",
+      senha: "123",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("E-mail é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de senha obrigatória", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 1,
+      nome: "Teste2",
+      email: "teste2@teste.com",
+      senha: "",
+      telefone: "99912345678",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Senha é obrigatória"));
+    }
+  });
+
+  it("Deve retornar erro de telefone obrigatório", async () => {
+    const pessoa: UpdatePessoaDTO = {
+      codigo: 1,
+      nome: "Teste2",
+      email: "teste2@teste.com",
+      senha: "123",
+      telefone: "",
+      admin: 0,
+    };
+
+    try {
+      await createPessoaController.exec(pessoa);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Telefone é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de código obrigatório", async () => {
+    try {
+      await getPessoaController.exec(0);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Código é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de código obrigatório", async () => {
+    try {
+      const codigo: any = "0";
+      await getPessoaController.exec(codigo);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Código inválido"));
+    }
+  });
+
+  it("Deve retornar erro de código obrigatório", async () => {
+    const codigo: any = undefined;
+
+    try {
+      await deletePessoaController.exec(codigo);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Código é obrigatório"));
+    }
+  });
+
+  it("Deve retornar erro de usuário não encontrado", async () => {
+    try {
+      await deletePessoaController.exec(3);
+
+      throw new Error("Falhou");
+    } catch (error) {
+      expect(error).toEqual(Error("Usuário não encontrado"));
+    }
+  });
+});
