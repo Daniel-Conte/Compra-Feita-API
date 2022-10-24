386cec82f04d3d2ef4068a495c15651b4e5885ec
Daniel Conte <182736@upf.br>
2022-10-24 09:53:56 -0300

    refactor(auth): :recycle: move expiracao do token para config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,5 @@ import { config } from "dotenv";
 config();
 
 export const authSecret = process.env.AUTH_SECRET as string;
+
+export const tokenExp = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 1; // 1 dia
--- src/controllers/Auth/index.ts
+++ src/controllers/Auth/index.ts
@@ -1,14 +1,11 @@
 import PasswordProviderBCrypt from "@providers/password/PasswordProviderBCrypt";
 import { pessoaRepositoryPostgresSQL } from "@controllers/Pessoa";
 import AuthTokenProviderJWT from "@providers/authToken/AuthTokenProviderJWT";
-import { authSecret } from "@config/index";
+import { authSecret, tokenExp } from "@config/index";
 import LoginController from "./LoginController";
 
 const passwordProviderBCrypt = new PasswordProviderBCrypt();
-const authTokenProviderJWT = new AuthTokenProviderJWT(
-  authSecret,
-  Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 1 // 1 dia
-);
+const authTokenProviderJWT = new AuthTokenProviderJWT(authSecret, tokenExp);
 
 export const loginController = new LoginController(
   pessoaRepositoryPostgresSQL,
